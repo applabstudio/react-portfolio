13448d882c3cd04326ef7a4427946c4660ce8894
silverdesignerdeveloper <silver.designer.developer@gmail.com>
2021-04-20 17:34:51 +0200

    commit start

---
+++ build/precache-manifest.5e648b0e686a469d72a87123fbb38672.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e869c3704c4a5e6798449374d246c09d",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "db52e8d74ed4b7724394",
+    "url": "/react-portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "bff2a37bcfade2f7dd35",
+    "url": "/react-portfolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "777bb5e64c26f5d33eb5",
+    "url": "/react-portfolio/static/css/main.59368cab.chunk.css"
+  },
+  {
+    "revision": "4bffc5c9629a31144877",
+    "url": "/react-portfolio/static/js/2.9daa8a13.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/react-portfolio/static/js/2.9daa8a13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db52e8d74ed4b7724394",
+    "url": "/react-portfolio/static/js/3.d149425b.chunk.js"
+  },
+  {
+    "revision": "bff2a37bcfade2f7dd35",
+    "url": "/react-portfolio/static/js/4.04bdea33.chunk.js"
+  },
+  {
+    "revision": "777bb5e64c26f5d33eb5",
+    "url": "/react-portfolio/static/js/main.cbdb48a2.chunk.js"
+  },
+  {
+    "revision": "1d287cceea6f34e40a66",
+    "url": "/react-portfolio/static/js/runtime-main.f8324f05.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/react-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/react-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/react-portfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/react-portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/react-portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/react-portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/react-portfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/react-portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/react-portfolio/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/react-portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/react-portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/react-portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/react-portfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/react-portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/react-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/react-portfolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/react-portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
